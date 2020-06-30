5e622d044478b3035f28e34527d954ad74c19aff
YannCharbon <y.charbon@bluewin.ch>
2020-06-30 13:42:39 +0200

    Added macro to make STANDBY mode to stay in TXPREP state to continuously emit carrier frequency

--- drivers/at86rf215/at86rf215_netdev.c
+++ drivers/at86rf215/at86rf215_netdev.c
@@ -196,7 +196,11 @@ static int _set_state(at86rf215_t *dev, netopt_state_t state)
 
     switch (state) {
         case NETOPT_STATE_STANDBY:
-            at86rf215_set_idle_from_rx(dev, CMD_RF_TRXOFF);
+            if (dev->params.carrier_mode == 0){     // Added by Yann Charbon
+                at86rf215_set_idle_from_rx(dev, CMD_RF_TRXOFF);     // Normal mode
+            } else {
+                at86rf215_rf_cmd(dev, CMD_RF_TXPREP);               // Go into TXPRED state to emit continuous carrier
+            }  
             break;
         case NETOPT_STATE_SLEEP:
             at86rf215_set_idle_from_rx(dev, CMD_RF_SLEEP);
